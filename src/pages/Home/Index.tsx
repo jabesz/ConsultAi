efb4f68853bca0a8c83a8cf28f7d4e5a3314b7d5
jabes <jabesvieira4@gmail.com>
2024-10-25 06:51:40 -0300

    update

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,31 +1,41 @@
+import styled from "styled-components";
 import Title from "../../components/Title/Index";
 import Banner from "../../components/Banner/Index";
 import Footer from "../../components/Footer/Index";
 import Header from "../../components/Header/Index";
 import Main from "../../components/Main/Index";
 
+const StyledTitle = styled(Title)`
+  position: absolute;
+  top: 36%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);
+  z-index: 1;
+  font-size: 3rem;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+    top: 40%; /* Ajuste de posição para telas menores */
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.5rem;
+    top: 45%; /* Ajuste adicional para telas ainda menores */
+  }
+`;
+
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
-      <Title
-        style={{
-          position: "absolute",
-          top: "36%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          color: "white",
-          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.6)",
-          zIndex: 1,
-        }}
-      >
-        Home
-      </Title>
+      <StyledTitle>Home</StyledTitle>
       <Main />
       <Footer />
     </>
   );
-}
-
+};
 export default Home;
